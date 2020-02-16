bc77e57cd64e5400bc799a988e366e3602294d62
Jairo Parra <jairoe.parra@gmail.com>
2020-02-16 12:44:29 -0500

    Fixed not importing db when running

--- app.py
+++ app.py
@@ -15,9 +15,6 @@ app.secret_key = 'jose'
 api = Api(app)
 
 
-@app.before_first_request
-def create_tables():
-    db.create_all()
 
 
 jwt = JWT(app, authenticate, identity)
@@ -31,4 +28,4 @@ api.add_resource(UserRegister, '/register')
 if __name__ == '__main__':
     from db import db
     db.init_app(app)
-    app.run(debug=True)
+    app.run(port=5000, debug=True)
